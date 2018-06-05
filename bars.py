d64b4ada71348f72584081c960ed4269a4d58b48
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-05 11:29:23 +0300

    release version

--- bars.py
+++ bars.py
@@ -1,46 +1,50 @@
 import json
 from haversine import haversine
 import sys
+import os
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='utf8') as file:
-        parsed = json.load(file)
-    return parsed
+    if os.path.exists(filepath):
+        with open(filepath, 'r', encoding='utf8') as file:
+            return json.load(file)
 
 
-def get_biggest_bar(bars_json):
-    bars_ammount = [i for i in range(len(bars_json['features']))]
-    seats_count_dict = {k: bars_json['features'][k]['properties']['Attributes']['SeatsCount'] for k in bars_ammount}
-    return bars_json['features'][max(seats_count_dict, key=seats_count_dict.get)]['properties']['Attributes']['Name']
+def get_biggest_bar(bars):
+    seats_count_dict = {k: bars['features'][k]['properties']['Attributes']['SeatsCount'] for k in
+                        range(len(bars['features']))}
+    return bars['features'][max(seats_count_dict, key=seats_count_dict.get)]['properties']['Attributes']['Name']
 
 
-def get_smallest_bar(bars_json):
-    bars_ammount = [i for i in range(len(bars_json['features']))]
-    seats_count_dict = {k: bars_json['features'][k]['properties']['Attributes']['SeatsCount'] for k in bars_ammount}
-    return bars_json['features'][min(seats_count_dict, key=seats_count_dict.get)]['properties']['Attributes']['Name']
+def get_smallest_bar(bars):
+    seats_count_dict = {k: bars['features'][k]['properties']['Attributes']['SeatsCount'] for k in
+                        range(len(bars['features']))}
+    return bars['features'][min(seats_count_dict, key=seats_count_dict.get)]['properties']['Attributes']['Name']
 
 
-def get_closest_bar(bars_json, longitude, latitude):
+def get_closest_bar(bars, longitude, latitude):
     my_coordinates = [longitude, latitude]
-    bars_ammount = [i for i in range(len(bars_json['features']))]
-    distances_dict = {k: haversine(my_coordinates, bars_json['features'][k]['geometry']['coordinates']) for k in bars_ammount}
-    return bars_json['features'][min(distances_dict, key=distances_dict.get)]['properties']['Attributes']['Name']
-
+    distances_dict = {k: haversine(my_coordinates, bars['features'][k]['geometry']['coordinates']) for k in
+                      range(len(bars['features']))}
+    return bars['features'][min(distances_dict, key=distances_dict.get)]['properties']['Attributes']['Name']
 
 
 if __name__ == '__main__':
-    bars_json = load_data("bars.json")
-    # if len(sys.argv) == 1:
-    #     print('Choose the operation: "get_biggest_bar", "get_smallest_bar" or "get_closest_bar"!')
-    # if ''.join(sys.argv[1]) == 'get_biggest_bar':
-    #     print(get_biggest_bar(bars_json))
-    # elif str(sys.argv[1]) == 'get_smallest_bar':
-    #     print(get_smallest_bar(bars_json))
-    # elif str(sys.argv[1]) =='get_closest_bar':
-    #     if str(sys.argv[2]) != None and str(sys.argv[3]) != None:
-    #         print(get_closest_bar(bars_json, sys.argv[2], sys.argv[3]))
-    #     else:
-    #         print('Set your longitude and latitude!')
-    # else:
-    #     print('Choose the operation: "get_biggest_bar", "get_smallest_bar" or "get_closest_bar"!')
+    if len(sys.argv) > 1:
+        try:
+            bars = load_data('bars.json')
+            if sys.argv[1] == 'get_biggest_bar':
+                print(get_biggest_bar(bars))
+            elif sys.argv[1] == 'get_smallest_bar':
+                print(get_smallest_bar(bars))
+            elif sys.argv[1] == 'get_closest_bar':
+                print(get_closest_bar(bars, float(sys.argv[2]), float(sys.argv[3])))
+        except IndexError:
+            print('Set your longitude and latitude!')
+        except ValueError:
+            print('ERROR: Check, that bars file exists and it is valid json file.')
+        except:
+            print('Unexpected error: ', sys.exc_info()[0])
+            raise
+    else:
+        print('Path to json file expected!')
