2c7cb7783086a70502f3e16e3dbde983b1f824dc
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-05 13:21:18 +0300

    simplify main method

--- bars.py
+++ bars.py
@@ -11,40 +11,43 @@ def load_data(filepath):
 
 
 def get_biggest_bar(bars):
-    seats_count_dict = {k: bars['features'][k]['properties']['Attributes']['SeatsCount'] for k in
-                        range(len(bars['features']))}
+    seats_count_dict = {bar_number: bars['features'][bar_number]['properties']['Attributes']['SeatsCount'] for
+                        bar_number in range(len(bars['features']))}
     return bars['features'][max(seats_count_dict, key=seats_count_dict.get)]['properties']['Attributes']['Name']
 
 
 def get_smallest_bar(bars):
-    seats_count_dict = {k: bars['features'][k]['properties']['Attributes']['SeatsCount'] for k in
-                        range(len(bars['features']))}
+    seats_count_dict = {bar_number: bars['features'][bar_number]['properties']['Attributes']['SeatsCount'] for
+                        bar_number in range(len(bars['features']))}
     return bars['features'][min(seats_count_dict, key=seats_count_dict.get)]['properties']['Attributes']['Name']
 
 
 def get_closest_bar(bars, longitude, latitude):
     my_coordinates = [longitude, latitude]
-    distances_dict = {k: haversine(my_coordinates, bars['features'][k]['geometry']['coordinates']) for k in
-                      range(len(bars['features']))}
+    distances_dict = {bar_number: haversine(my_coordinates, bars['features'][bar_number]['geometry']['coordinates']) for
+                      bar_number in range(len(bars['features']))}
     return bars['features'][min(distances_dict, key=distances_dict.get)]['properties']['Attributes']['Name']
+
+
+def print_result(bars):
+    if sys.argv[1] == 'get_biggest_bar':
+        print(get_biggest_bar(bars))
+    elif sys.argv[1] == 'get_smallest_bar':
+        print(get_smallest_bar(bars))
+    elif sys.argv[1] == 'get_closest_bar':
+        print(get_closest_bar(bars, float(sys.argv[2]), float(sys.argv[3])))
+    else:
+        print('ERROR: Unknown operation type.')
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         try:
             bars = load_data('bars.json')
-            if sys.argv[1] == 'get_biggest_bar':
-                print(get_biggest_bar(bars))
-            elif sys.argv[1] == 'get_smallest_bar':
-                print(get_smallest_bar(bars))
-            elif sys.argv[1] == 'get_closest_bar':
-                print(get_closest_bar(bars, float(sys.argv[2]), float(sys.argv[3])))
+            print_result(bars)
         except IndexError:
-            print('Set your longitude and latitude!')
+            print('ERROR: Set your longitude and latitude.')
         except ValueError:
-            print('ERROR: Check, that bars file exists and it is valid json file.')
-        except:
-            print('Unexpected error: ', sys.exc_info()[0])
-            raise
+            print('ERROR: Check your coordinates type: float expected.')
     else:
-        print('Path to json file expected!')
+        print('ERROR: Operation expected.')
