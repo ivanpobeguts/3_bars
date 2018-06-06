96c4165314e8d3983c06ad4e75ecb3506b492a70
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-06-06 13:26:34 +0300

    move command_map to function

--- bars.py
+++ bars.py
@@ -22,35 +22,40 @@ def get_closest_bar(bars, longitude, latitude):
 
 
 def print_bar(bar):
-    print('Name: ', bar['properties']['Attributes']['Name'])
-    print('Address: ', bar['properties']['Attributes']['Address'])
-    print('Area: ', bar['properties']['Attributes']['AdmArea'])
-    print('District: ', bar['properties']['Attributes']['District'])
-    print('Phone: ', bar['properties']['Attributes']['PublicPhone'][0]['PublicPhone'])
-    print('Seats count: ', bar['properties']['Attributes']['SeatsCount'])
-    print('Coordinates: ', bar['geometry']['coordinates'])
+        print('Name: ', bar['properties']['Attributes']['Name'])
+        print('Address: ', bar['properties']['Attributes']['Address'])
+        print('Area: ', bar['properties']['Attributes']['AdmArea'])
+        print('District: ', bar['properties']['Attributes']['District'])
+        print('Phone: ', bar['properties']['Attributes']['PublicPhone'][0]['PublicPhone'])
+        print('Seats count: ', bar['properties']['Attributes']['SeatsCount'])
+        print('Coordinates: ', bar['geometry']['coordinates'])
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('filepath', help='Path to json file')
-    parser.add_argument('command', choices=function_map.keys())
+    parser.add_argument(
+        'command', choices=get_command_map().keys())
     parser.add_argument('longitude', type=float, nargs='?', const=None)
     parser.add_argument('latitude', type=float, nargs='?', const=None)
     return parser
 
 
-if __name__ == '__main__':
-    function_map = {'get_biggest_bar': get_biggest_bar,
+def get_command_map():
+    command_map = {'get_biggest_bar': get_biggest_bar,
                     'get_smallest_bar': get_smallest_bar,
                     'get_closest_bar': get_closest_bar}
+    return command_map
+
+
+if __name__ == '__main__':
     args = get_parser().parse_args()
     try:
         bars = load_data(args.filepath)['features']
         if args.command == 'get_closest_bar' and args.longitude and args.latitude:
-            bar = function_map[args.command](bars, args.longitude, args.latitude)
+            bar = get_command_map()[args.command](bars, args.longitude, args.latitude)
         else:
-            bar = function_map[args.command](bars)
+            bar = get_command_map()[args.command](bars)
         print_bar(bar)
     except json.decoder.JSONDecodeError:
         print('File is not json.')
